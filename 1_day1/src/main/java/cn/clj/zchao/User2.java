4a371cab2ef2f9a5d101a6042c7a32631f8d1c15
229028988@qq.com <z1571312541C>
2019-05-05 10:00:52 +0800

    1

---
+++ 1_day1/src/main/java/cn/clj/zchao/User2.java
@@ -0,0 +1,37 @@
+package cn.clj.zchao;
+
+
+import lombok.*;
+
+/**
+ * 〈〉
+ *
+ * @author 22902
+ * @create 2018/12/7
+ */
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class User2 {
+
+    private String name;
+
+    private Integer age;
+
+    private Integer height;
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
